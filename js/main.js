9c23f7745317010f81ecd9dd78492e87dce2ebd2
SarvarRahim <tesla_19@mail.ru>
2021-09-06 18:36:46 +0500

    add already exicts contact

--- js/main.js
+++ js/main.js
@@ -1,50 +1,83 @@
-var elForm = document.querySelector('.js-form');
-var elFormName = document.querySelector('.js-form-name');
-var elFormReletionship = document.querySelector('.js-form-reletionship');
-var elFormNumber = document.querySelector('.js-form-number');
-var elContactsList = document.querySelector('.js-contact-list');
-var contactsArray = []
-var id = 0;
+// Kontaktlarni joylash uchun bo'sh array ochamiz
+var contacts = [];
 
-if (elForm) {
-  elForm.addEventListener('submit', function (evt){
+
+// DOM
+var elNewContactForm = document.querySelector('.js-new-contact-form');
+var elNewContactNameInput = elNewContactForm.querySelector('.js-new-contact-name-input');
+var elNewContactRelationshipInput = elNewContactForm.querySelector('.js-new-contact-relationship-input');
+var elNewContactPhoneInput = elNewContactForm.querySelector('.js-new-contact-phone-input');
+
+var elContacts = document.querySelector('.contacts');
+var elContactsList = elContacts.querySelector('.contacts__list');
+var elWarningPhone = document.querySelector('.js-warning-text');
+
+
+
+function addContact() {
+  contacts.push({
+    name: elNewContactNameInput.value.trim(),
+    relationship: elNewContactRelationshipInput.value.trim(),
+    phone: elNewContactPhoneInput.value.trim()
+  });
+}
+
+
+function showContacts() {
+  elContactsList.innerHTML = '';
+  var elContactsFragment = document.createDocumentFragment();
+
+  for (var contact of contacts) {
+    var elNewLi = document.createElement('li');
+    elNewLi.classList.add('list-group-item', 'p-3');
+
+    var elNewHeading = document.createElement('h3');
+    elNewHeading.classList.add('h5');
+    elNewHeading.textContent = contact.name;
+    elNewLi.appendChild(elNewHeading);
+
+    var elNewRelDiv = document.createElement('div');
+    elNewRelDiv.classList.add('mb-2');
+    elNewRelDiv.textContent = contact.relationship;
+    elNewLi.appendChild(elNewRelDiv);
+
+    var elNewPhoneLink = document.createElement('a');
+    elNewPhoneLink.classList.add('btn', 'btn-outline-success', 'btn-sm');
+    elNewPhoneLink.href = contact.phone;
+    elNewPhoneLink.textContent = contact.phone;
+    elNewLi.appendChild(elNewPhoneLink);
+    elContactsFragment.appendChild(elNewLi);
+    elContactsList.appendChild(elContactsFragment);
+  }
+}
+
+// Form bo'lsa
+if (elNewContactForm) {
+  elNewContactForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
-    var contactItem = document.createElement('li');
-    var contactName = document.createElement('h3');
-    var contactRelation = document.createElement('p');
-    var contactNumber = document.createElement('a');
-    var contact = {
-      id : null,
-      name : null,
-      relation : null,
-      phone : null
+    var phoneIndex = contacts.findIndex(function(contact){
+      return contact.phone === elNewContactPhoneInput.value.trim();
+    });
+
+    if (phoneIndex > -1){
+      elNewContactPhoneInput.classList.add('is-invalid');
+      elWarningPhone.classList.remove('d-none');
+      return;
     }
+    addContact();
+    // Add contact to contacts array
 
-    contactName.textContent = elFormName.value;
-    contactRelation.textContent = elFormReletionship.value;
-    contactNumber.textContent = elFormNumber.value;
 
-    contact.id = id;
-    contact.name = elFormName.value;
-    contact.relation = elFormReletionship.value;
-    contact.phone = elFormNumber.value;
+    // Reset inputs
+    elNewContactNameInput.value = '';
+    elNewContactRelationshipInput.value = '';
+    elNewContactPhoneInput.value = '';
 
-    elFormName.value = '';
-    elFormReletionship.value = '';
-    elFormNumber.value = '';
-
-    contactItem.appendChild(contactName);
-    contactItem.appendChild(contactRelation);
-    contactItem.appendChild(contactNumber);
-
-    contactItem.classList.add('bg-white', 'w-100', 'p-3', 'rounded', 'border', 'mb-4');
-    contactNumber.classList.add('text-decoration-none');
-    contactNumber.href="tel:" + elFormNumber.value
-
-    elContactsList.appendChild(contactItem);
-    contactsArray.push(contact);
-
+    showContacts();
+    // addContact funksiyasi yordamida yangi kontaktni contactsga qo'shiladi
+    // showContacts funksiyasi yordamida HTMLda kontaktlarni ko'rsatamiz
 
   });
 }
+// Form submitda amal bajariladi
